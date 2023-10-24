921cd81a298725298b84b2fdbb5acd5f3ade2c8f
essel.dev <info@essel.dev>
2023-10-24 17:00:03 +0200

    docs: Dynamic version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,13 +4,16 @@ For the full list of built-in configuration values, see the documentation:
 https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
+# https://setuptools-scm.readthedocs.io/en/latest/usage/#usage-from-sphinx
+from importlib.metadata import version as get_version
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project: str = 'JewCal'
-project_copyright: str = '2023, essel.dev'
+project_copyright: str = 'essel.dev'
 author: str = 'essel.dev'
-release: str = '0.4'
+release: str = get_version(project)
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
